bd62f392c113fb0bc0f1999030a19f4f9e5b69cf
Erwin Müller <erwin.mueller@deventm.org>
2017-11-11 18:43:24 +0100

    Initial commit.

---
+++ image/rootfs/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+source /docker-entrypoint-utils.sh
+set_debug
+echo "Running as `id`"
+
+sync_dir /opt/gitlab/embedded/service/gitlab-rails/public.dist /opt/gitlab/embedded/service/gitlab-rails/public
+
+exec ${BASH_CMD} -- "$@"
