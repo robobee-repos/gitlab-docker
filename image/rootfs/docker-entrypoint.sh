5a18ce83b163d87221b7d5aa78b5eba6010b589f
Erwin Müller <erwin.mueller@deventm.org>
2017-11-13 17:06:06 +0100

    Add readme. Set version v10.1.2-ce.0-r1.

--- image/rootfs/docker-entrypoint.sh
+++ image/rootfs/docker-entrypoint.sh
@@ -5,6 +5,6 @@ source /docker-entrypoint-utils.sh
 set_debug
 echo "Running as `id`"
 
-sync_dir /opt/gitlab/embedded/service/gitlab-rails/public.dist /opt/gitlab/embedded/service/gitlab-rails/public
+sync_dir "/opt/gitlab/embedded/service/gitlab-rails/public.dist" "/opt/gitlab/embedded/service/gitlab-rails/public"
 
 exec ${BASH_CMD} -- "$@"
